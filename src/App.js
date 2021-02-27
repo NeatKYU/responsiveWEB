fbb6a1f8d26da93667dcc6e4f22626ceb35cbac7
ninjasKYU <2066apple@naver.com>
2021-02-27 13:17:52 +0900

    responsive project with media query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+//components
+import Header from './components/Header';
+import Aside from './components/Aside';
+import Footer from './components/Footer';
+import Section from './components/Section';
+
+//css
+import './css/App.css';
+import "./css/Section.css";
+import './css/Header.css';
+import "./css/Footer.css";
+import "./css/Aside.css";
+import "./css/Article.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header></Header>
+			<Aside></Aside>
+			<Section /* 3개의 article이 들어있음 */></Section>
+			<Footer></Footer>
+		</div>
+	)
 }
 
 export default App;
---
+++ src/components/Section.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Article from './Article';
+
+function Section() {
+	return (
+		<div className="section">
+			<Article/>
+			<Article/>
+			<Article/>
+		</div>
+	);
+}
+
+export default Section;
